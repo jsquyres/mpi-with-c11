16f46f4a979502b605e633fc5a24d7baa7d8d5a1
Jeff Squyres <jsquyres@cisco.com>
2015-12-09 09:05:34 -0800

    Add some strings for printing

--- test_mpi_send.c
+++ test_mpi_send.c
@@ -20,17 +20,17 @@ typedef uint64_t MPI_Count;
 /******************************/
 
 
-int MPI_Send(const void *buf, int count, MPI_Datatype datatype, int dest, int tag, MPI_Comm comm)
+int MPI_Send(const char *buf, int count, MPI_Datatype datatype, int dest, int tag, MPI_Comm comm)
 {
-   printf("MPI_Send\n");
-   return MPI_SUCCESS;
+    printf("MPI_Send (%s): %d\n", buf, count);
+    return MPI_SUCCESS;
 }
 
 
-int MPI_Send_x(const void *buf, MPI_Count count, MPI_Datatype datatype, int dest, int tag, MPI_Comm comm)
+int MPI_Send_x(const char *buf, MPI_Count count, MPI_Datatype datatype, int dest, int tag, MPI_Comm comm)
 {
-   printf("MPI_Send_x\n");
-   return MPI_SUCCESS;
+    printf("MPI_Send_x (%s): %llu\n", buf, count);
+    return MPI_SUCCESS;
 }
 
 
@@ -50,28 +50,35 @@ int MPI_Send_x(const void *buf, MPI_Count count, MPI_Datatype datatype, int dest
 
 int main(void)
 {
-   MPI_Send(NULL,(short)0,MPI_BYTE,0,0,MPI_COMM_WORLD);
+    short s = 3;
+    MPI_Send("short",s,MPI_BYTE,0,0,MPI_COMM_WORLD);
+    s = -3;
+    MPI_Send("short neg",s,MPI_BYTE,0,0,MPI_COMM_WORLD);
 
-   MPI_Send(NULL,0,MPI_BYTE,0,0,MPI_COMM_WORLD);
-   MPI_Send(NULL,0L,MPI_BYTE,0,0,MPI_COMM_WORLD);
-   MPI_Send(NULL,0LL,MPI_BYTE,0,0,MPI_COMM_WORLD);
+    MPI_Send("int const",17,MPI_BYTE,0,0,MPI_COMM_WORLD);
+    MPI_Send("long const",18L,MPI_BYTE,0,0,MPI_COMM_WORLD);
+    MPI_Send("long long const",19LL,MPI_BYTE,0,0,MPI_COMM_WORLD);
 
-   MPI_Send(NULL,(unsigned)0,MPI_BYTE,0,0,MPI_COMM_WORLD);
+    // 2^31+1
+    unsigned u = 2147483649;
+    MPI_Send("unsigned",u,MPI_BYTE,0,0,MPI_COMM_WORLD);
 
-   MPI_Send(NULL,(unsigned long)0,MPI_BYTE,0,0,MPI_COMM_WORLD);
-   MPI_Send(NULL,(unsigned long long)0,MPI_BYTE,0,0,MPI_COMM_WORLD);
+    // 2^63+1
+    unsigned long ul = 9223372036854775809UL;
+    MPI_Send("unsigned long",ul,MPI_BYTE,0,0,MPI_COMM_WORLD);
+    unsigned long long ull = 9223372036854775809ULL;
+    MPI_Send("unsigned long long",ull,MPI_BYTE,0,0,MPI_COMM_WORLD);
 
-   MPI_Send(NULL,(MPI_Aint)0,MPI_BYTE,0,0,MPI_COMM_WORLD);
-   MPI_Send(NULL,(MPI_Count)0,MPI_BYTE,0,0,MPI_COMM_WORLD);
-   MPI_Count c = 0;
-   MPI_Aint  a = 0;
-   MPI_Send(NULL,a,MPI_BYTE,0,0,MPI_COMM_WORLD);
-   MPI_Send(NULL,c,MPI_BYTE,0,0,MPI_COMM_WORLD);
+    MPI_Aint a = 27;
+    MPI_Send("MPI_Aint",a,MPI_BYTE,0,0,MPI_COMM_WORLD);
 
-   const int x = 3;
-   MPI_Send(NULL,x,MPI_BYTE,0,0,MPI_COMM_WORLD);
-   volatile int y = 3;
-   MPI_Send(NULL,y,MPI_BYTE,0,0,MPI_COMM_WORLD);
+    MPI_Count c = 9223372036854775809ULL;
+    MPI_Send("MPI_Count",c,MPI_BYTE,0,0,MPI_COMM_WORLD);
 
-   return 0;
+    const int x = 3;
+    MPI_Send("const int",x,MPI_BYTE,0,0,MPI_COMM_WORLD);
+    volatile int y = 3;
+    MPI_Send("volatile int",y,MPI_BYTE,0,0,MPI_COMM_WORLD);
+
+    return 0;
 }
