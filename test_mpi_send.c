0bee1bfd8c87cd29e93485a824d7fb32f742c550
Jeff Hammond <jeff.science@gmail.com>
2015-12-09 07:51:36 -0800

    print out type info too

--- test_mpi_send.c
+++ test_mpi_send.c
@@ -50,28 +50,41 @@ int MPI_Send_x(const void *buf, MPI_Count count, MPI_Datatype datatype, int dest
 
 int main(void)
 {
-   MPI_Send(NULL,(short)0,MPI_BYTE,0,0,MPI_COMM_WORLD);
+    printf("(short)0...");
+    MPI_Send(NULL,(short)0,MPI_BYTE,0,0,MPI_COMM_WORLD);
 
-   MPI_Send(NULL,0,MPI_BYTE,0,0,MPI_COMM_WORLD);
-   MPI_Send(NULL,0L,MPI_BYTE,0,0,MPI_COMM_WORLD);
-   MPI_Send(NULL,0LL,MPI_BYTE,0,0,MPI_COMM_WORLD);
+    printf("0...");
+    MPI_Send(NULL,0,MPI_BYTE,0,0,MPI_COMM_WORLD);
+    printf("0L...");
+    MPI_Send(NULL,0L,MPI_BYTE,0,0,MPI_COMM_WORLD);
+    printf("0LL...");
+    MPI_Send(NULL,0LL,MPI_BYTE,0,0,MPI_COMM_WORLD);
 
-   MPI_Send(NULL,(unsigned)0,MPI_BYTE,0,0,MPI_COMM_WORLD);
+    printf("(unsigned)0...");
+    MPI_Send(NULL,(unsigned)0,MPI_BYTE,0,0,MPI_COMM_WORLD);
 
-   MPI_Send(NULL,(unsigned long)0,MPI_BYTE,0,0,MPI_COMM_WORLD);
-   MPI_Send(NULL,(unsigned long long)0,MPI_BYTE,0,0,MPI_COMM_WORLD);
+    printf("(unsigned long)0...");
+    MPI_Send(NULL,(unsigned long)0,MPI_BYTE,0,0,MPI_COMM_WORLD);
+    printf("(unsigned long long)0...");
+    MPI_Send(NULL,(unsigned long long)0,MPI_BYTE,0,0,MPI_COMM_WORLD);
 
-   MPI_Send(NULL,(MPI_Aint)0,MPI_BYTE,0,0,MPI_COMM_WORLD);
-   MPI_Send(NULL,(MPI_Count)0,MPI_BYTE,0,0,MPI_COMM_WORLD);
-   MPI_Count c = 0;
-   MPI_Aint  a = 0;
-   MPI_Send(NULL,a,MPI_BYTE,0,0,MPI_COMM_WORLD);
-   MPI_Send(NULL,c,MPI_BYTE,0,0,MPI_COMM_WORLD);
+    printf("(MPI_Aint)0...");
+    MPI_Send(NULL,(MPI_Aint)0,MPI_BYTE,0,0,MPI_COMM_WORLD);
+    printf("(MPI_Count)0...");
+    MPI_Send(NULL,(MPI_Count)0,MPI_BYTE,0,0,MPI_COMM_WORLD);
+    MPI_Count c = 0;
+    MPI_Aint  a = 0;
+    printf("MPI_Count c = 0...");
+    MPI_Send(NULL,a,MPI_BYTE,0,0,MPI_COMM_WORLD);
+    printf("MPI_Aint  a = 0...");
+    MPI_Send(NULL,c,MPI_BYTE,0,0,MPI_COMM_WORLD);
 
-   const int x = 3;
-   MPI_Send(NULL,x,MPI_BYTE,0,0,MPI_COMM_WORLD);
-   volatile int y = 3;
-   MPI_Send(NULL,y,MPI_BYTE,0,0,MPI_COMM_WORLD);
+    const int x = 3;
+    printf("const int x = 3...");
+    MPI_Send(NULL,x,MPI_BYTE,0,0,MPI_COMM_WORLD);
+    printf("volatile int y = 3...");
+    volatile int y = 3;
+    MPI_Send(NULL,y,MPI_BYTE,0,0,MPI_COMM_WORLD);
 
-   return 0;
+    return 0;
 }
