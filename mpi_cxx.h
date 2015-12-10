07c4be17600e2a19b0315fecff4c2579e419fa77
Jeff Hammond <jeff.science@gmail.com>
2015-12-09 21:32:23 -0800

    use templates to fix type promotion issues

--- mpi_cxx.h
+++ mpi_cxx.h
@@ -5,8 +5,20 @@
 #error do not include this file directly; include mpi.h
 #endif
 
-int MPI_Send(const char *buf, int count, MPI_Datatype datatype, int dest, int tag, MPI_Comm comm);
+int MPI_Send(const void *buf, int count, MPI_Datatype datatype, int dest, int tag, MPI_Comm comm)
+{
+    return MPI_SUCCESS;
+}
 
-int MPI_Send(const char *buf, MPI_Count count, MPI_Datatype datatype, int dest, int tag, MPI_Comm comm);
+int MPI_Send(const void *buf, MPI_Count count, MPI_Datatype datatype, int dest, int tag, MPI_Comm comm)
+{
+    return MPI_SUCCESS;
+}
+
+template <class T>
+int MPI_Send(const void *buf, T count, MPI_Datatype datatype, int dest, int tag, MPI_Comm comm)
+{
+    return MPI_Send(buf,static_cast<MPI_Count>(count),datatype,dest,tag,comm);
+}
 
 #endif // MPI_CXX_H
